bd92594bc913d2ff41b42a48549aa9d0cc401a92
Ugo Pellissier de Féligonde <ugo.defeli@gmail.com>
2021-02-04 16:07:31 +0100

    resize image test in perfect square + comment area & comment section finishing

--- frontend/assets/js/vue.js
+++ frontend/assets/js/vue.js
@@ -4,18 +4,29 @@ const postglobals = [
         post: {
             userpicture: 'assets/img/tests/Hector_Castor.jpg',
             username: "Hector Castor",
+            date: "1 juil. 2020, 8h32",
             img: 'assets/img/tests/homer_simpson_bush.gif',
             like: 24,
             dislike: 3,
             comment: 8,
             fav: 3
         },
-        comments: [{
-            id: 122,
-            username: "Edouard Canard",
-            userpicture : 'assets/img/tests/Edouard_Canard.jpg',
-            comment: "MDR Simpson qui recule dans buisson"
-        }]
+        comments:[
+            {
+                id: 1,
+                username: "Edouard Canard",
+                userpicture : 'assets/img/tests/Edouard_Canard.jpeg',
+                date: "2 juil. 2020, 10h43",
+                content: "MDR Homer Simpson qui recule dans les buissons"
+            },
+            {
+                id: 2,
+                username: "Charlotte Marmotte",
+                userpicture : 'assets/img/tests/Charlotte_Marmotte.jpg',
+                date: "2 juil. 2020, 10h51",
+                content: "Edouard tu comptes décrire chaque post à chaque fois ?? Je te taquine... ahah.. ce GIF.. comme nos concurrents quand ils nous voient sur le salon tmtc ;)"
+            }
+        ]
     }
 ]
 
