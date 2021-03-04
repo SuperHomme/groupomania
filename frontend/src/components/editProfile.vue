b11c35470c11fddaa45144d49888fb516187580f
Ugo Pellissier de Féligonde <ugo.defeli@gmail.com>
2021-03-04 18:58:21 +0100

    front : updatedUserPicture logic : OK

--- frontend/src/components/editProfile.vue
+++ frontend/src/components/editProfile.vue
@@ -4,8 +4,14 @@
     <form action="" method="get">
 
         <div class="user__picture">
-            <img v-bind:src="user.userpicture">
-            <i class="fas fa-plus-square"></i>
+
+            <img v-bind:src="imagePreview">
+            
+            <input v-on:change="onFileChange" class="file" type="file" id="file" name="file">
+            <label for="file">
+                <i class="fas fa-plus-square"></i>
+            </label>
+
         </div>
 
         <h2>{{actualUsername}}</h2>
@@ -78,6 +84,11 @@ export default {
             user: {},
             actualUsername: '',
             actualEmail: '',
+            errorSize: "",
+            errorType: "",
+            legendPending: "",
+            imagePending: "", // imagePending sous forme de file
+            imagePreview: "", // image decrite en base64
             wantChangePassword: false,
             loginUserId: JSON.parse(localStorage.getItem("vuex")).account.userId,
             loginToken: JSON.parse(localStorage.getItem("vuex")).account.token,
@@ -90,7 +101,72 @@ export default {
                     .then(user => {
                         this.user = user.data,
                         this.actualUsername = this.user.username,
-                        this.actualEmail = this.user.email })
+                        this.actualEmail = this.user.email,
+                        this.imagePreview= this.user.userpicture })
+                    .catch((error) => console.log(error));
+        },
+        // eslint-disable-next-line no-unused-vars
+        onFileChange(e) {
+            this.removeImage(); // on enlève d'abords l'image éventuellement déjà présente (quant on veut changer d'image)
+            // eslint-disable-next-line no-unused-vars
+            let files = e.target.files || e.dataTransfer.files; // disons que files est égal à ces deux façons de récupérer la donnée, sélection et drag&drop
+            if (!files.length) // si cette donnée n'existe pas, 
+                return; // on arrête la fonction ici
+            this.checkImage(files[0]);  // on prend le premier elt de l'array files
+        },
+        checkImage(file) {
+            this.imagePending = file;
+            if (file.size >= 2000000 || file.type != "image/gif") {
+                if (file.size >= 2000000) { // si la taille du fichier dépasse 2 million de bytes = 2 MB
+                    const bytes = this.formatBytes(file.size);
+                    this.errorSize = "ce fichier est trop lourd (limite 2Mo) car il pèse "  + bytes;
+                }
+                if (file.type != "image/gif") { // si le type du fichier n'est pas un GIF
+                    this.errorType = "ce fichier n'est pas un GIF, c'est un "  + file.type.substr(file.type.search("/")+1);
+                }
+                console.log("là, y a un pb sur l'image");
+                return;
+            }
+            this.createImage(file); // si l'image est conforme, on crée l'image
+        },
+        createImage(file) {
+            // eslint-disable-next-line no-unused-vars
+            let imagePreview = new Image(); // constructeur Image. cela devient une balise <img>
+            let reader = new FileReader(); // constucteur FileReader
+            let vm = this; // astuce pour éviter une confusion sur les références this
+            // eslint-disable-next-line no-unused-vars
+            reader.onload = (e) => {
+                // eslint-disable-next-line no-unused-vars
+                vm.imagePreview = e.target.result; // le résultat est envoyé dans imagePreview
+            };
+            reader.readAsDataURL(file); // la méthode readAsDataURL permet de lire le contenu de l’objet file. à la fin de l'opération de lecture, l’attribut result contient les données dans une URL représentant les données du fichier sous forme de chaîne encodée en base64.
+        },
+        // eslint-disable-next-line no-unused-vars
+        removeImage: function (e) { // pour supprimer l'image, (pas d'arrow function, sinon pas de this)
+            console.log("image preview supprimée");
+            this.imagePending = ''; // on remet à zero les variables
+            this.imagePreview = '';
+            this.errorSize = "";
+            this.errorType = "";
+        },
+        formatBytes(bytes, decimals = 2) {
+        if (bytes === 0) return '0 Bytes';
+        const k = 1000; // should be 1024, but whatever
+        const dm = decimals < 0 ? 0 : decimals;
+        const sizes = ['octets', 'ko', 'Mo', 'Go', 'To', 'Po', 'Eo', 'Zo', 'Yo']; // Bytes in french is octets
+        const i = Math.floor(Math.log(bytes) / Math.log(k));
+        return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+        },
+        createUpdatedUserPicture() {
+            const updatedUserPicture = new FormData(); // img & date sont créés en backend
+            updatedUserPicture.append("image", this.imagePending); // imagePending, c'est file, soit files[0], cad l'image sous forme pure de fichier. et multer attend un single.('image')
+            // console.log(this.imagePending);
+            this.sendUpdatedUserPicture(updatedUserPicture);
+        },
+        sendUpdatedUserPciture(updatedUserPicture) {
+            axios
+                .put('http://localhost:3000/api/users/' + this.loginUserId + '/picture', updatedUserPicture, { headers: { Authorization: "Bearer " + this.loginToken }} )
+                    .then(console.log("user infos modifiées !"))
                     .catch((error) => console.log(error));
         },
         // TODO : check user input + notification
@@ -151,14 +227,20 @@ export default {
         margin: 1rem
         height: 250px
         position: relative
-        font-size: 24px
         img
             width: 250px
             border-radius: 50%
-        i
+        .file
+            display: none
             position: absolute
             right: 0
             bottom: 0
+            margin: 0.5rem
+        i
+            font-size: 24px
+            color: rgb(51, 204, 255)
+            cursor: pointer
+            
     &__password
         display: flex
         flex-direction: column
